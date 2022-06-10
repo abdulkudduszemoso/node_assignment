b897ec8f49b57364dbee3a98d4b78af0625c7265
abdulkudduszemosolabs <abdul.kuddus@zemosolabs.com>
2022-06-10 10:24:24 +0530

    Change database

--- utils/db.config.js
+++ utils/db.config.js
@@ -2,6 +2,6 @@ export const dbConfig = {
     host: 'localhost',
     user: 'root',
     password: 'Abdul@123',
-    database: 'bc_31_database_service',
+    database: 'email_Service',
     dialect: 'mysql'
 }
